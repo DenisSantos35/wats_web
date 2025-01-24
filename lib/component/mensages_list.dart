6497bd37111a66e1e0231153e1e939958945e1dd
Denis <denisdiogo18@hotmail.com>
2025-01-24 09:42:32 -0300

    criação da listagem de mensagens remetente e destinatario

--- lib/component/mensages_list.dart
+++ lib/component/mensages_list.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:wats_web/models/user.dart';
@@ -17,10 +19,16 @@ class MensagesList extends StatefulWidget {
 }
 
 class _MensagesListState extends State<MensagesList> {
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
   TextEditingController _mensageController = TextEditingController();
   late Users _recipientUser;
   late Users _sendUser;
-  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //ficara ouvindo  para alterar os dados a cada nova mensagem
+  StreamController _streamController =
+      StreamController<QuerySnapshot>.broadcast();
+  //quando sair da conversa ele cancelara o ouvinte.
+  late StreamSubscription _streamMensages;
 
   _sendMensage() {
     String mensageText = _mensageController.text;
@@ -35,6 +43,9 @@ class _MensagesListState extends State<MensagesList> {
       //salvar mensagem para remetente
       String recipientUserId = _recipientUser.idUser;
       _saveMensage(recipientUserId, sendUserId, mensage);
+
+      //salvar mensagem para o destinatario
+      _saveMensage(sendUserId, recipientUserId, mensage);
     }
   }
 
@@ -48,9 +59,42 @@ class _MensagesListState extends State<MensagesList> {
     _mensageController.clear();
   }
 
+  /*
+    estrutura das mensagens
+    mensagens
+      + Denis
+        + Marcos
+          + mensagem (Denis)
+      
+      + Marcos
+        + Denis
+          + mensagem (Denis)
+    
+   */
+
   _recoverInitialData() {
     _sendUser = widget.sendUser;
     _recipientUser = widget.recipientUser;
+
+    _mensageListenerAdd();
+  }
+
+  _mensageListenerAdd() {
+    final stream = _firestore
+        .collection("mensages")
+        .doc(_sendUser.idUser)
+        .collection(_recipientUser.idUser)
+        .orderBy("data", descending: false)
+        .snapshots();
+    _streamMensages = stream.listen((data) {
+      _streamController.add(data);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _streamMensages.cancel();
   }
 
   @override
@@ -72,11 +116,64 @@ class _MensagesListState extends State<MensagesList> {
         ),
         child: Column(children: [
           //mensage list
-          Expanded(
-              child: Container(
-                  width: size.width,
-                  color: Colors.orange,
-                  child: const Text("Lista mensagem"))),
+          StreamBuilder(
+              stream: _streamController.stream,
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return const Expanded(
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Text("Carregando dados"),
+                            CircularProgressIndicator()
+                          ],
+                        ),
+                      ),
+                    );
+                  case ConnectionState.active:
+                  case ConnectionState.done:
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text("Erro ao carregar dados"),
+                      );
+                    } else {
+                      QuerySnapshot querySnapshot = snapshot.data;
+                      List<DocumentSnapshot> mensagesList =
+                          querySnapshot.docs.toList();
+                      return Expanded(
+                        child: ListView.builder(
+                            itemCount: querySnapshot.docs.length,
+                            itemBuilder: (context, index) {
+                              DocumentSnapshot mensage = mensagesList[index];
+                              Alignment alignment = Alignment.bottomLeft;
+                              Color color = Colors.white;
+
+                              if (_sendUser.idUser == mensage["idUser"]) {
+                                alignment = Alignment.bottomRight;
+                                color = PaleteColors.backgroundColorCarMensage;
+                              }
+
+                              return Align(
+                                alignment: alignment,
+                                child: Container(
+                                  //define o maximo de largura que o container pode ter
+                                  //ele recebe um size.
+                                  constraints: BoxConstraints.loose(size * 0.8),
+                                  margin: EdgeInsets.all(6),
+                                  decoration: BoxDecoration(
+                                      color: color,
+                                      borderRadius: BorderRadius.circular(8)),
+                                  padding: EdgeInsets.all(16),
+                                  child: Text(mensage["text"]),
+                                ),
+                              );
+                            }),
+                      );
+                    }
+                }
+              }),
 
           //box of the text
           Container(
--- lib/utils/palete_colors.dart
+++ lib/utils/palete_colors.dart
@@ -5,4 +5,5 @@ class PaleteColors {
   static const Color primaryColor = Color(0xff075E54);
   static const Color destaqueColor = Color(0xff25D366);
   static const Color backgroundColorBar = Color(0xffededed);
+  static const Color backgroundColorCarMensage = Color(0xffd2ffa5);
 }
